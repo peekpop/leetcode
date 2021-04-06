67eebbe47000ab53424b4ff7645d1d0afea82882
assuse <peekpop@126.com>
2021-04-06 13:12:32 +0800

    add SumToNum

---
+++ src/main/java/com/assuse/arithmetic/SumToNum.java
@@ -0,0 +1,29 @@
+package com.assuse.arithmetic;
+
+import java.util.Scanner;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ */
+public class SumToNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String next = input.next();
+        System.out.println(next);
+
+    }
+
+    /**
+     * @param nums   输入的数组
+     * @param target 目标值
+     * @return 返回nums 索引值
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        return new int[2];
+    }
+}
